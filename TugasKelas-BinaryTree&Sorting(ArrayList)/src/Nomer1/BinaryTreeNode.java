9bd6d5cd8ac35c3ad40429f802ea45171437fd76
Riki <Riki@DESKTOP-31403HU>
2017-12-10 18:17:23 +0700

    Commite TugasKelas-BinaryTree&Sorting(ArrayLlist)

---
+++ TugasKelas-BinaryTree&Sorting(ArrayList)/src/Nomer1/BinaryTreeNode.java
@@ -0,0 +1,18 @@
+package Nomer1;
+
+
+public class BinaryTreeNode {
+    Object element;
+    BinaryTreeNode leftChild;
+    BinaryTreeNode rightChild;
+    public BinaryTreeNode(){}
+    public BinaryTreeNode(Object theElement){
+        element = theElement;
+    }
+    public BinaryTreeNode(Object theElement,BinaryTreeNode theleftChild,BinaryTreeNode therightChild){
+        element = theElement;
+        leftChild = theleftChild;
+        rightChild = therightChild;
+    }
+    
+}
